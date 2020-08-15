081440a34768f4f0422732f36a2fd8bddc1393ff
deannapi <deannapi.mart@gmail.com>
2020-08-14 19:07:06 -0500

    initials folders

---
+++ src/components/header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Nav(propr) {
+    const {
+
+    } = props;
+
+    return (
+        <>
+            <div>
+                <h1>Honey Do List</h1>
+                <h3></h3>
+            </div>
+            <nav>
+
+            </nav>
+        </>
+    )
+}
