c0fad058e3283f85ce616a4f2b0a2918a640c316
deannapi <deannapi.mart@gmail.com>
2020-08-20 21:32:21 -0500

    added group create page and its mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,4 +1,4 @@
-import gql from 'graphql-tag';
+import gql from "graphql-tag";
 
 export const LOGIN = gql`
   mutation login($email: String!, $password: String!) {
@@ -12,8 +12,42 @@ export const LOGIN = gql`
 `;
 
 export const ADD_USER = gql`
-  mutation addUser($firstName: String!, $lastName: String!, $email: String!, $password: String!) {
-    addUser(firstName: $firstName, lastName: $lastName, email: $email, password: $password) {
+  mutation addUser(
+    $firstName: String!
+    $lastName: String!
+    $username: String!
+    $email: String!
+    $password: String!
+  ) {
+    addUser(
+      firstName: $firstName
+      lastName: $lastName
+      username: $username
+      email: $email
+      password: $password
+    ) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
+
+export const CREATE_GROUP = gql`
+  mutation createGroup($groupName: String!, $groupPassword: String!) {
+    createGroup(groupName: $groupName, groupPassword: $groupPassword) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
+
+export const JOIN_GROUP = gql`
+  mutation joinGroup($groupName: String!, $groupPassword: String!) {
+    joinGroup(groupName: $groupName, groupPassword: $groupPassword) {
       token
       user {
         _id
