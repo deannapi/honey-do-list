d1364f0c662e864e33fbae0105d508378eb64e21
Chris Short <61039651+durcoorigin@users.noreply.github.com>
2020-08-20 21:36:10 -0500

    service workers and manifest

---
+++ client/build/precache-manifest.a94842613bd191ebe39804eb24a4fa47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6920450b3b907adb5c160908e98fd30e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c6fabcd2e1c9652f3f9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "43501989e7057970db35",
+    "url": "/static/js/2.422eb2cc.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.422eb2cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c6fabcd2e1c9652f3f9",
+    "url": "/static/js/main.4fc9dd22.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
