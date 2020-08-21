a305971517a6c06ed6496cc58b9228d084778153
Chris Short <61039651+durcoorigin@users.noreply.github.com>
2020-08-21 18:30:16 -0500

    build Join.js

---
+++ client/src/pages/Join.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { useMutation } from '@apollo/react-hooks';
+import { Link } from "react-router-dom";
+import { JOIN_GROUP } from "../utils/mutations"
+import Auth from "../utils/auth";
+
+function JoinGroup(props) {
+    const [formState, setFormState] = useState({ email: '', password: '' });
+    const [joinGroup] = useMutation(JOIN_GROUP);
+
+const handleFormSubmit = async event => {
+    event.preventDefault();
+    const mutationResponse = await joinGroup({
+      variables: {
+        email: formState.email, password: formState.password,
+      }
+    });
+    const token = mutationResponse.data.joinGroup.token;
+    Auth.login(token);
+  };
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value
+    });
+  };
+
+  return (
+    <div className="container my-1">
+      <Link to="/join">
+        ← Go to Join
+      </Link>
+
+      <h2>Join Group</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="email">Email:</label>
+          <input
+            placeholder="youremail@test.com"
+            name="email"
+            type="email"
+            id="email"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="grpPwd">Group Password:</label>
+          <input
+            placeholder="must be at least 6 characters"
+            name="password"
+            type="password"
+            id="grpPwd"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row flex-end">
+          <button type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+
+}
+
+export default JoinGroup;
