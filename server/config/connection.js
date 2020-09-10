19a4296e4b68adce58e7f05c1dcca6c34a3171d1
deannapi <deannapi.mart@gmail.com>
2020-09-10 14:51:39 -0500

    adding mongodb atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,14 +1,20 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/honeydolist', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-}).then(() => {
-  console.log('MongoDB connection successful.');
-}).catch((e) => {
-  console.log('MongoDB connection error: ', e.message)
-});
+const connection = `mongodb+srv://deannapi:Mart3141@cluster0.c7ofa.mongodb.net/honey-do-list?retryWrites=true&w=majority`
+
+mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+    .then(() => console.log("Database Connected Successfully"))
+    .catch(err => console.log(err));
+
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/honeydolist', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false
+// }).then(() => {
+//   console.log('MongoDB connection successful.');
+// }).catch((e) => {
+//   console.log('MongoDB connection error: ', e.message)
+// });
 
 module.exports = mongoose.connection;
